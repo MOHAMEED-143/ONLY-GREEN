7eac54ae515478b37163e0b7a7d1b0b9a0afe1b6
MOHAMED-143 <158847209+MOHAMEED-143@users.noreply.github.com>
2024-02-07 16:14:10 +0100

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ try:
 except:os.system("pip uninstall requests -y;pip install requests")
  
 print('\033[1;91m[◽\033[1;91m] \033[1;92mChecking For Update...')
-os.system('git pull --quiet 2>/dev/null')
+os.system('git pull')
 ranaxd = platform.architecture()[0]
 if ranaxd == '64bit':
  print('\033[1;91m[◽\033[1;91m] \033[1;92mYour Device is 64bit')
