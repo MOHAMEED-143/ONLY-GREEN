2167b5b56ea989ee1020623296e3a168f03333d8
MOHAMED-143 <158847209+MOHAMEED-143@users.noreply.github.com>
2024-04-16 10:29:51 +0100

    Update run.py

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ os.system('git pull')
 ranaxd = platform.architecture()[0]
 if ranaxd == '64bit':
  print('\033[1;91m[◽\033[1;91m] \033[1;92mYour Device is 64bit')
- import MOHAMED
+ import MAHADI
 elif ranaxd == '32bit':
  print('\033[1;91m[◽\033[1;91m] \033[1;92mYour Devive is 32bit')
  exit() 
