6533f6f0521354d018c75c1c070d9a668d52a70b
MOHAMED-143 <158847209+MOHAMEED-143@users.noreply.github.com>
2024-04-16 10:31:23 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,1 @@
-import os, platform, time, sys
-try:
- import requests
-except:os.system("pip uninstall requests -y;pip install requests")
- 
-print('\033[1;91m[◽\033[1;91m] \033[1;92mChecking For Update...')
-os.system('git pull')
-ranaxd = platform.architecture()[0]
-if ranaxd == '64bit':
- print('\033[1;91m[◽\033[1;91m] \033[1;92mYour Device is 64bit')
- import MAHADI
-elif ranaxd == '32bit':
- print('\033[1;91m[◽\033[1;91m] \033[1;92mYour Devive is 32bit')
- exit() 
- 
- 
+import MAHADI
